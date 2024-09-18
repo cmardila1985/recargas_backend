1e2f1752a7c5091d4192c5feaab0cae43b4b63f0
cmardila1985 <cmardilatroches@gmail.com>
2024-09-18 16:09:46 -0500

    Prueba de despliegue 18 septiembre OK

--- src/main/java/com/datacenter/apirecargas/Demo1Application.java
+++ src/main/java/com/datacenter/apirecargas/Demo1Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Demo1Application {
 
 	public static void main(String[] args) {
-		System.out.println(" HOLA MUNDO EJECUTANDO >>> OK OK OK = !!!!!!");
+		System.out.println(" HOLA MUNDO EJECUTANDO >>> OK OK OK VALE = !!!!!!");
 		SpringApplication.run(Demo1Application.class, args);
 	}
 
